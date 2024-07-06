0f23ffa9a17164c26076b60cd4aeb93e7f4e2327
HIR0-728 <70585820+HIR0-728@users.noreply.github.com>
2024-07-06 15:45:47 +0900

    add: Chapter4までのコードを反映

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,10 +1,15 @@
-import Layout from "components/layout"
+
+import Container from "components/container"
 import Hero from "components/hero"
 
 export default function Home() {
-  return <Hero
-    title="CUBE"
-    subtitle="アウトプットしていくサイト"
-    imageOn
-  />
+  return (
+    <Container>
+      <Hero
+        title="CUBE"
+        subtitle="アウトプットしていくサイト"
+        imageOn
+      />
+    </Container>
+  )
 }
